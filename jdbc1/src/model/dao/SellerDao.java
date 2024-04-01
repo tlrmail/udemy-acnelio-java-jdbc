ea00c8130eb96ca2529c18372a4f66bf387624fe
thiago.leite <thiago.leite@dataprev.gov.br>
2024-04-01 11:10:23 -0300

    DepartmentDao and SellerDao interfaces

---
+++ jdbc1/src/model/dao/SellerDao.java
@@ -0,0 +1,20 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public interface SellerDao {
+
+	public void insert(Seller seller);
+
+	public void update(Seller seller);
+
+	public void deletebyId(Integer id);
+
+	public Department findById(Integer id);
+
+	public List<Seller> findAll();
+
+}
