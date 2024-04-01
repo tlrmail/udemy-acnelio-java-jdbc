8fb6d4e44bbecead8f2a7b4278e53cc890a9d5b2
thiago.leite <thiago.leite@dataprev.gov.br>
2024-04-01 07:59:03 -0300

    Project created

---
+++ jdbc1/src/db/DB.java
@@ -0,0 +1,75 @@
+package db;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import db.execeptions.DbException;
+
+public class DB {
+
+	private static Connection connection = null;
+
+	public static Connection getConnection() throws DbException {
+		if (connection == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				connection = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return connection;
+	}
+
+	public static void closeConnection() throws DbException{
+		if(connection != null) {
+			try {
+				connection.close();
+			}catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			}catch(SQLException e) {
+				System.out.println("Error: " + e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeStatement(Statement st) {
+		if(st != null) {
+			try {
+				st.close();
+			}catch(SQLException e) {
+				System.out.println("Error: " + e.getMessage());
+			}
+		}
+	}
+	
+	private static Properties loadProperties() throws DbException {
+
+		try (FileInputStream fs = new FileInputStream(new File("db.properties"))) {
+
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+
+	}
+
+}
---
+++ jdbc1/src/db/execeptions/DbIntegrityException.java
@@ -0,0 +1,14 @@
+package db.execeptions;
+
+public class DbIntegrityException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DbIntegrityException(String msg) {
+		super(msg);
+	}
+	
+}
