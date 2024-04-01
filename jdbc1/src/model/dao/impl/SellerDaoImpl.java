93d647c27e86e99a2d84f710640d2bce94146911
thiago.leite <thiago.leite@dataprev.gov.br>
2024-04-01 11:18:38 -0300

    SellerDaoImpl and DaoFactory

---
+++ jdbc1/src/model/dao/impl/SellerDaoImpl.java
@@ -0,0 +1,36 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class SellerDaoImpl implements SellerDao{
+
+	@Override
+	public void insert(Seller seller) {
+		
+	}
+
+	@Override
+	public void update(Seller seller) {
+		
+	}
+
+	@Override
+	public void deletebyId(Integer id) {
+		
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		return null;
+	}
+
+}
