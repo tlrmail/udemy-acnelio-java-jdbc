128086819ac0d05f3864576095e0c5cd0486fb8e
thiago.leite <thiago.leite@dataprev.gov.br>
2024-04-01 08:36:40 -0300

    Seller entitiy class

--- jdbc1/src/application/Program005.java
+++ jdbc1/src/application/Program005.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program005 {
 
 	public static void main(String[] args) {
 		
 		Department department = new Department(1, "Books");
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, department);
+
 		System.out.println(department);
+		System.out.println(seller);
 		
 	}
 }
